996bfa9f83ffb4f03ca07131899ac434c54bffce
Theanh2911 <89338550+Theanh2911@users.noreply.github.com>
2023-05-12 21:14:02 +0700

    initial upload

---
+++ public/chat.js
@@ -0,0 +1,33 @@
+$(function () {
+    //make connection
+    var socket = io.connect('http://localhost:3000');
+    //buttons and inputs
+    var message = $("#message")
+    var username = $("#username")
+    var send_message = $("#send_message")
+    var send_username = $("#send_username")
+    var chatroom = $("#chatroom")
+    var feedback = $("#feedback")
+    //Emit message
+    send_message.click(function () {
+        socket.emit('new_message', {message: message.val()})
+    })
+    //listen on new_mesage
+    socket.on("new_message", (data) => {
+        feedback.html('');
+        message.val('');
+        chatroom.append("<p class='message'>" + data.username + ": " + data.message + "</p>")
+    })
+
+    send_username.click(function () {
+        socket.emit('change_username', {username: username.val()})
+    })
+    //emit typing
+    message.bind("keypress", () => {
+        socket.emit('typing')
+    })
+    //listen on typing
+    socket.on('typing', (data) => {
+        feedback.html("<p><i>" + data.username + " is typing a message..." + "</i></p>")
+    })
+});
